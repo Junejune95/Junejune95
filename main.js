a14671ea948e83b260432e8cdf442b2256eb4735
zune <seinmama95@gmail.com>
2022-07-09 23:23:27 +0630

    add techical skill

--- main.js
+++ main.js
@@ -1,35 +1,50 @@
-$(function() {
-  
-    var link = $('.menu .vertical__menu li');
-    // Move to specific section when click on menu link
-    link.on('click', function(e) {
-      var target = $($(this).attr('href'));
-      $('html, body').animate({
-        scrollTop: target.offset().top
-      }, 600);
-      $(this).addClass('active');
-      e.preventDefault();
-    });
-    
-    // Run the scrNav when scroll
-    $(window).on('scroll', function(){
-      scrNav();
-    });
-    
-    // scrNav function 
-    // Change active dot according to the active section in the window
-    function scrNav() {
-      var sTop = $(window).scrollTop();
-      $('section').each(function() {
-        var id = $(this).attr('id'),
-            offset = $(this).offset().top-30,
-            height = $(this).height();
-            
-        if(sTop >= offset && sTop < offset + height) {
-          link.removeClass('active');
-          $('.menu .vertical__menu').find('[data-scroll="' + id + '"]').addClass('active');
-        }
-      });
-    }
+$(function () {
+
+  var link = $('.menu .vertical__menu li');
+  // Move to specific section when click on menu link
+  link.on('click', function (e) {
+    var target = $($(this).attr('href'));
+    $('html, body').animate({
+      scrollTop: target.offset().top
+    }, 600);
+    $(this).addClass('active');
+    e.preventDefault();
+  });
+
+  // Run the scrNav when scroll
+  $(window).on('scroll', function () {
     scrNav();
   });
+
+  // scrNav function 
+  // Change active dot according to the active section in the window
+  function scrNav() {
+    var sTop = $(window).scrollTop();
+    $('section').each(function () {
+      var id = $(this).attr('id'),
+        offset = $(this).offset().top - 30,
+        height = $(this).height();
+
+      if (sTop >= offset && sTop < offset + height) {
+        link.removeClass('active');
+        $('.menu .vertical__menu').find('[data-scroll="' + id + '"]').addClass('active');
+      }
+    });
+  }
+  scrNav();
+
+  var coll = document.getElementsByClassName("collapsible");
+  for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function () {
+      this.classList.toggle("active");
+      var content = this.nextElementSibling;
+      if (content.style.maxHeight) {
+        content.style.maxHeight = null;
+      } else {
+        content.style.maxHeight = content.scrollHeight + "px";
+      }
+    });
+  }
+});
+
+
